ceec6a262743d7de95e4aeb3a884d871d268e31b
fakel_ast <lvov-styopa@ya.ru>
2021-02-23 20:52:19 +0300

    added object personalMovieBD and questions for fill him

--- module_1/js/script.js
+++ module_1/js/script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+const personalMovieBD = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastViewedFilm = prompt('Какой ваш последний просмотренный фильм?', ''),
+    scoreLastViewedFilm = prompt('Как вы его оцените?', '');
+
+personalMovieBD.movies[lastViewedFilm] = scoreLastViewedFilm;
+
+lastViewedFilm = prompt('Какой ваш последний просмотренный фильм?', '');
+scoreLastViewedFilm = prompt('Как вы его оцените?', '');
+
+personalMovieBD.movies[lastViewedFilm] = scoreLastViewedFilm;
+
+console.log(personalMovieBD);
